74bc1914f71687419abee1bb6129ac8f484b6f17
George Oddoye <50216284+Goddoye@users.noreply.github.com>
2020-04-15 06:52:55 -0400

    R Basics

---
+++ ifelseBasics.R
@@ -0,0 +1,18 @@
+#If else, and else if statements
+x = 15
+if (x==10){
+  print ('X is equal to 10')
+} else if (x==12){
+  print('X is equal to 12')
+} else {
+  print("X is not equal to 10 or 12")
+}
+
+hot <- FALSE
+temp <- 60
+
+if (temp >80){
+  print("It is hot")
+}else{
+  print("It is not hot")
+}
